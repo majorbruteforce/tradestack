5687ec5c5655febc4d97f86565bea5f3f6963cc1
majorbruteforce <saswat.pb03@gmail.com>
2025-07-15 22:41:35 +0530

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# TradeStack
+
+A simple and fast implementation of an order book in C++ for 
+back testing, simulations and real-time trading applications.
+
+### Build
+```bash
+git clone https://github.com/majorbruteforce/tradestack.git
+cd tradestack
+mkdir build && cd build
+cmake ..
+make
+```
---
+++ build/CMakeFiles/tradestack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tradestack.dir/src/main.cpp.o"
+  "CMakeFiles/tradestack.dir/src/main.cpp.o.d"
+  "tradestack"
+  "tradestack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tradestack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
